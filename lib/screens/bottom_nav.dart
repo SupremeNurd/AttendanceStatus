5a50e0ef40b33d166e0c13fc290dee99ef142b55
WABS <min008@hanmail.net>
2022-12-30 18:07:56 +0900

    Initial commit

--- lib/screens/bottom_nav.dart
+++ lib/screens/bottom_nav.dart
@@ -11,23 +11,8 @@ class _AttendanceStatus extends State<AttendanceStatus> {
   int _selectedIndex = 0;
   String navImage = 'assets/images/bottom_nav_center.png';
 
-<<<<<<< HEAD
   static const TextStyle optionStyle =
       TextStyle(fontSize: 20, fontWeight: FontWeight.bold);
-
-  static const List<Widget> _widgetOptions = <Widget>[
-    Text(
-      '페이지1',
-      style: optionStyle,
-    ),
-    Text(
-      '출석신청 현황',
-      textAlign: TextAlign.left, style: optionStyle,
-=======
-  static const TextStyle optionStyle = TextStyle(
-    fontSize: 20,
-    fontWeight: FontWeight.bold,
-  );
 
   static final List<Widget> _widgetOptions = <Widget>[
     Text(
@@ -90,7 +75,6 @@ class _AttendanceStatus extends State<AttendanceStatus> {
           ),
         ),
       ],
->>>>>>> f589bc0 (Initial commit)
     ),
     Text(
       '현장출석 QR페이지',
@@ -116,120 +100,94 @@ class _AttendanceStatus extends State<AttendanceStatus> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        leading: IconButton(
-          onPressed: () {},
-<<<<<<< HEAD
-          icon: Icon(Icons.add, size: 30, color: Colors.red,),
-=======
-          icon: Icon(
-            Icons.add,
-            size: 30,
-            color: Colors.red,
+        leading: Padding(
+          padding: const EdgeInsets.only(left: 20),
+          child: IconButton(
+            onPressed: () {},
+            icon: Icon(
+              Icons.arrow_back,
+              size: 30,
+              color: Colors.black,
+            ),
           ),
->>>>>>> f589bc0 (Initial commit)
         ),
         actions: [
-          IconButton(
-            onPressed: () {},
-<<<<<<< HEAD
-            icon: Icon(Icons.add, size: 30, color: Colors.red,),
-=======
-            icon: Icon(
-              Icons.add,
-              size: 30,
-              color: Colors.red,
+          Padding(
+            padding: const EdgeInsets.only(right: 20),
+            child: IconButton(
+              onPressed: () {},
+              icon: Image.asset(
+                'assets/images/alert.png',
+                width: 30,
+                height: 30,
+              ),
             ),
->>>>>>> f589bc0 (Initial commit)
           ),
         ],
         backgroundColor: Colors.white,
         elevation: 0,
-<<<<<<< HEAD
-
-=======
->>>>>>> f589bc0 (Initial commit)
       ),
       body: Column(
         children: [
           Center(
             child: _widgetOptions.elementAt(_selectedIndex),
           ),
-<<<<<<< HEAD
-          Container(
-            height: 140,
-            width: 400,
-            color: Colors.grey,
-            margin: EdgeInsets.all(10),
-          ),
-          Container(
-            height: 140,
-            width: 400,
-            color: Colors.grey,
-            margin: EdgeInsets.all(10),
-          ),
-          Container(
-            height: 140,
-            width: 400,
-            color: Colors.grey,
-            margin: EdgeInsets.all(10),
-          ),
-=======
->>>>>>> f589bc0 (Initial commit)
         ],
       ),
       bottomNavigationBar: Container(
-          padding: const EdgeInsets.all(8.0),
-          child: ClipRRect(
-            borderRadius: BorderRadius.circular(30),
-            child: BottomNavigationBar(
-              type: BottomNavigationBarType.fixed,
-              backgroundColor: Colors.black,
-              selectedItemColor: Colors.white,
-              unselectedItemColor: Colors.white.withOpacity(.60),
-              showSelectedLabels: false,
-              showUnselectedLabels: false,
-              currentIndex: _selectedIndex, //현재 선택된 Index
-              onTap: _onItemTapped,
-              items: [
-                BottomNavigationBarItem(
-                  icon: Icon(
-                    Icons.home,
-                    size: 35,
-                  ),
-                  label: '01',
+        padding: const EdgeInsets.all(8.0),
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(30),
+          child: BottomNavigationBar(
+            type: BottomNavigationBarType.fixed,
+            backgroundColor: Colors.black,
+            selectedItemColor: Colors.white,
+            unselectedItemColor: Colors.white.withOpacity(.60),
+            showSelectedLabels: false,
+            showUnselectedLabels: false,
+            currentIndex: _selectedIndex, //현재 선택된 Index
+            onTap: _onItemTapped,
+            items: [
+              BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.home,
+                  size: 35,
                 ),
-                BottomNavigationBarItem(
-                  icon: Icon(
-                    Icons.article_outlined,
-                    size: 35,
-                  ),
-                  label: '02',
+                label: '01',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.article_outlined,
+                  size: 35,
                 ),
-                BottomNavigationBarItem(
-                  icon: Image.asset(
-                    'assets/images/bottom_nav_center.png',
-                    width: 65,
-                    height: 65,
-                  ),
-                  label: '03',
+                label: '02',
+              ),
+              BottomNavigationBarItem(
+                icon: Image.asset(
+                  'assets/images/bottom_nav_center.png',
+                  width: 65,
+                  height: 65,
                 ),
-                BottomNavigationBarItem(
-                  icon: Icon(
-                    Icons.calendar_month,
-                    size: 35,
-                  ),
-                  label: '04',
+                label: '03',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.calendar_month,
+                  size: 35,
                 ),
-                BottomNavigationBarItem(
-                  icon: Icon(
-                    Icons.account_circle,
-                    size: 35,
-                  ),
-                  label: '05',
+                label: '04',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.account_circle,
+                  size: 35,
                 ),
-              ],
-            ),
-          )),
+                label: '05',
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
